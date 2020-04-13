e5f59ca7a907d2f870f9401f77d848ab8c2d7e6d
Pirijan <hi@pirijan.com>
2020-04-13 12:50:17 -0400

    upgrade 11ty version, remove unused plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
-
 module.exports = function(config) {
   config.addPassthroughCopy("./assets")
-  config.addPlugin(eleventyNavigationPlugin)
 
   return {
     templateFormats: [
