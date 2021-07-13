2ccad82592482e801d70d416a4d0a1e5bc136147
Pirijan <hi@pirijan.com>
2021-07-13 14:55:16 -0400

    split up search and paint js files

--- assets/js/magic-paint.js
+++ assets/js/magic-paint.js
@@ -1,87 +1,3 @@
-console.log('🍍 search ready')
-
-// Search
-
-let params = (new URL(document.location)).searchParams
-let search = params.get("search")
-const searchIcon = document.querySelector('.search #search')
-const removeIcon = document.querySelector('.search #remove')
-const searchForm = document.querySelector('.search form')
-const searchInput = document.querySelector('.search input')
-const helloSection = document.querySelector('section#hello')
-const headers = document.querySelectorAll('section#index h2')
-const posts = document.querySelectorAll('section#index li')
-
-if (search) {
-  searchInput.value = search
-  filterPage(search)
-}
-
-if (searchInput) {
-  searchIcon.addEventListener ('click', () => {
-    searchInput.focus()
-  })
-  removeIcon.addEventListener ('click', () => {
-    clearFilter()
-  })
-  searchInput.addEventListener ('input', (event) => {
-    const value = searchInput.value
-    if (value) {
-      filterPage(value)
-    } else {
-      clearFilter()
-    }
-  })
-  searchForm.addEventListener ('submit', (event) => {
-    event.preventDefault()
-    const value = searchInput.value
-    const searchUrl = `${window.location.origin}?search=${value}`
-    window.location.href = searchUrl
-  })
-}
-
-function filterPage (value) {
-  if (!helloSection) { return }
-  helloSection.classList.add('hidden')
-  filterHeaders(value)
-  filterPosts(value)
-}
-
-function filterHeaders (value) {
-  headers.forEach(header => {
-    let postsList = header.dataset.posts
-    postsList = postsList.split(',').filter(Boolean)
-    const results = fuzzy.filter(value, postsList)
-    if (results.length === 0) {
-      header.classList.add('hidden')
-    } else {
-      header.classList.remove('hidden')
-    }
-  })
-}
-
-function filterPosts (value) {
-  posts.forEach(post => {
-    let postTitle = post.dataset.title
-    const results = fuzzy.filter(value, [postTitle])
-    if (results.length === 0) {
-      post.classList.add('hidden')
-    } else {
-      post.classList.remove('hidden')
-    }
-  })
-}
-
-function clearFilter () {
-  searchInput.value = ""
-  if (!helloSection) { return }
-  headers.forEach(header => header.classList.remove('hidden'))
-  posts.forEach(post => post.classList.remove('hidden'))
-  helloSection.classList.remove('hidden')
-}
-
-
-// Drawing
 // code adapted from https://k-komma.de/assets/js/main.js
 
 let canvas, context, canvasImage, color, prevScroll
@@ -99,7 +15,7 @@ let isTouch = false
 randomColor()
 initCanvas()
 initPageCanvas()
-console.log('🌸 drawing ready')
+console.log('🌸 magic paint ready')
 
 function initCanvas () {
   canvas = document.getElementById('background')
@@ -251,7 +167,8 @@ window.onscroll = function (event) {
 }
 
 const resizeObserver = new ResizeObserver(entries => {
-  console.log('Body height changed', entries[0].target.clientHeight)
+  // console.log('Body height changed', entries[0].target.clientHeight)
+  console.log('canvas height updated')
   updateCanvas()
 })
 resizeObserver.observe(document.body)
